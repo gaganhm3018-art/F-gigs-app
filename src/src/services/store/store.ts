15368d426eb10412a0ebf0c8a2ad54c5603dd502
Nikhil677 <nikhilsepta472@gmail.com>
2026-05-08 02:15:56 +0530

    del the extra files

--- src/src/services/store/store.ts
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import incomeReducer from './incomeSlice';
-
-export const store = configureStore({
-  reducer: { income: incomeReducer },
-});
-
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
